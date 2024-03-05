aa75c55d88eff9756454c7eb619603623803091f
SHIVAANISREE <rkshivaanisree2003@gmail.com>
2024-03-05 14:52:45 +0530

    Calculator

---
+++ Calculator/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [result, setResult] = useState('');
+
+  function clickhandler(event) {
+    setResult(result.concat(event.target.value));
+  }
+
+  const cleardisplay = () => {
+    setResult('');
+  };
+
+  const calculate = () => {
+    setResult(eval(result).toString());
+  }
+
+  return (
+    <div className="calc">
+      <input type='text' placeholder='0' id='answer' value={result}></input>
+      <input type='button' value={"9"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"8"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"7"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"6"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"5"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"4"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"3"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"2"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"1"} className='button' onClick={clickhandler}></input>
+ 
+      <input type='button' value={"0"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"+"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"-"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"*"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"/"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"%"} className='button' onClick={clickhandler}></input>
+      <input type='button' value={"."} className='button' onClick={clickhandler}></input>
+
+      <input type='button' value={"Clear"} className='button button1' onClick={cleardisplay}></input>
+      <input type='button' value={"="} className='button button1' onClick={calculate}></input>
+    </div>
+  );
+}
+
+export default App;
